d54500e5454bfdf27723b83fca0aaa2fffa8e420
John Wästerlund <grunka@gmail.com>
2012-09-29 10:35:05 +0200

    Creating the PRNG part

--- src/main/java/se/grunka/fortuna/Fortuna.java
+++ src/main/java/se/grunka/fortuna/Fortuna.java
@@ -1,33 +1,56 @@
 package se.grunka.fortuna;
 
 import java.util.Random;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Fortuna extends Random {
+    private static final int MIN_POOL_SIZE = 64;
+
+    private long lastReseedTime = 0;
+    private long reseedCount = 0;
+    private final Generator generator;
+    private final Pool[] pools;
+
     public static Fortuna createInstance() {
-        Fortuna fortuna = new Fortuna();
-        fortuna.initialize();
-        return fortuna;
+        Pool[] pools = new Pool[32];
+        for (int pool = 0; pool < pools.length; pool++) {
+            pools[pool] = new Pool();
+        }
+        return new Fortuna(new Generator(), pools);
     }
 
-    private final ExecutorService executorService = Executors.newCachedThreadPool();
-
-    Fortuna() {
+    private Fortuna(Generator generator, Pool[] pools) {
+        this.generator = generator;
+        this.pools = pools;
     }
 
-    private void initialize() {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            @Override
-            public void run() {
-                executorService.shutdown();
+    private byte[] randomData(int bytes) {
+        if (pools[0].size() >= MIN_POOL_SIZE && System.currentTimeMillis() - lastReseedTime > 100) {
+            reseedCount++;
+            byte[] seed = new byte[0];
+            for (int pool = 0; pool < pools.length; pool++) {
+                //TODO all this...
             }
-        });
+            generator.reseed(seed);
+        }
+        if (reseedCount == 0) {
+            throw new IllegalStateException("Generator not reseeded yet");
+        } else {
+            return generator.pseudoRandomData(bytes);
+        }
     }
+
+    //TODO seed file management
 
     @Override
     protected int next(int bits) {
+        //TODO get bytes and put into int...
         return 0;
+    }
+
+    @Override
+    public void nextBytes(byte[] bytes) {
+        //TODO check if this is doable
+        super.nextBytes(bytes);
     }
 
     @Override
