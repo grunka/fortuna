d638153f88f9041a982180e6e03d1d415eae1a3e
John Wästerlund <grunka@gmail.com>
2012-10-23 08:31:10 +0200

    Using read/write locks instead

--- src/main/java/se/grunka/fortuna/Pool.java
+++ src/main/java/se/grunka/fortuna/Pool.java
@@ -2,13 +2,15 @@ package se.grunka.fortuna;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class Pool {
-    private final ReentrantLock lock = new ReentrantLock();
-    private final AtomicInteger size = new AtomicInteger(0);
+    private final ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+    private final Lock readLock = lock.readLock();
+    private final Lock writeLock = lock.writeLock();
     private final MessageDigest poolDigest = createDigest();
+    private long size = 0;
 
     private MessageDigest createDigest() {
         try {
@@ -18,12 +20,17 @@ public class Pool {
         }
     }
 
-    public int size() {
-        return size.get();
+    public long size() {
+        readLock.lock();
+        try {
+            return size;
+        } finally {
+            readLock.unlock();
+        }
     }
 
     public void add(int source, byte[] event) {
-        lock.lock();
+        writeLock.lock();
         try {
             if (source < 0 || source > 255) {
                 throw new IllegalArgumentException("Source needs to be in the range 0 to 255, it was " + source);
@@ -31,21 +38,21 @@ public class Pool {
             if (event.length < 1 || event.length > 32) {
                 throw new IllegalArgumentException("The length of the event need to be in the range 1 to 32, it was " + event.length);
             }
-            size.addAndGet(event.length + 2);
+            size += event.length + 2;
             poolDigest.update(new byte[]{(byte) source, (byte) event.length});
             poolDigest.update(event);
         } finally {
-            lock.unlock();
+            writeLock.unlock();
         }
     }
 
     public byte[] getAndClear() {
-        lock.lock();
+        writeLock.lock();
         try {
-            size.set(0);
+            size = 0;
             return poolDigest.digest();
         } finally {
-            lock.unlock();
+            writeLock.unlock();
         }
     }
 }
