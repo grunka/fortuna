d2205fe68974f250ce14580c7787200eace9a288
John Wästerlund <grunka@gmail.com>
2019-02-15 11:21:28 +0100

    Test verifying that the prefetcher doesn't serve outputs in the wrong order

--- src/test/java/com/grunka/random/fortuna/PrefetchingSupplierTest.java
+++ src/test/java/com/grunka/random/fortuna/PrefetchingSupplierTest.java
@@ -4,18 +4,25 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.Assert.assertEquals;
 
 public class PrefetchingSupplierTest {
     private ExecutorService executorService;
+    private List<Integer> sleeps;
 
     @Before
     public void setUp() {
-        executorService = Executors.newFixedThreadPool(2);
+        sleeps = new ArrayList<>(Arrays.asList(200, 150, 100, 50, 0));
+        executorService = Executors.newFixedThreadPool(5);
     }
 
     @After
@@ -30,5 +37,31 @@ public class PrefetchingSupplierTest {
         assertEquals("hello 0", prefetcher.get());
         assertEquals("hello 1", prefetcher.get());
         assertEquals("hello 2", prefetcher.get());
+    }
+
+    @Test
+    public void shouldBeOrderedAndCorrectNumberOfOutputs() throws ExecutionException, InterruptedException {
+        AtomicInteger number = new AtomicInteger();
+        PrefetchingSupplier<Integer> prefetcher = new PrefetchingSupplier<>(() -> {
+            sleep();
+            return number.getAndIncrement();
+        }, executorService);
+        List<Integer> values = new ArrayList<>();
+        List<Future<?>> futures = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            futures.add(executorService.submit(() -> values.add(prefetcher.get())));
+        }
+        for (Future<?> future : futures) {
+            future.get();
+        }
+        assertEquals(Arrays.asList(0, 1, 2, 3, 4), values);
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(sleeps.remove(0));
+        } catch (InterruptedException e) {
+            throw new Error(e);
+        }
     }
 }
